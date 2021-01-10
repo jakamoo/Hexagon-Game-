00ae671e56046bcddc7460de432a322b80ab50d9
Sinan <51867024+jakamoo@users.noreply.github.com>
2021-01-10 06:16:01 +0300

    Add files via upload

---
+++ game_play.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+#include "functions.h"
+#include <string>
+
+using namespace std;
+
+
+
+
+int main(){
+
+vector <Hex> games;
+Menu menu1(games);
+menu1.mainMenu();
+
+
+}
+
